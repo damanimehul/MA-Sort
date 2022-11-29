f879759f508705af8c07aa634fec15a4c77d45f7
damanimehul <48509266+damanimehul@users.noreply.github.com>
2022-11-28 22:22:43 -0500

    try higher lr

---
+++ run_scripts/twoagents-v5_2022-11-28/runner.sh
@@ -0,0 +1,2 @@
+mkdir /home/gridsan/mdamani/6.7950-project/slurm_jobs/twoagents-v5
+sbatch job.slurm 
---
+++ run_scripts/twoagents-v6_2022-11-28/runner.sh
@@ -0,0 +1,2 @@
+mkdir /home/gridsan/mdamani/6.7950-project/slurm_jobs/twoagents-v6
+sbatch job.slurm 
